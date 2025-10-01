8524eef3cfef562107df08396f9b449389fee600
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2025-10-01 19:35:58 +0900

    fix

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://tsumiage.magicgifted.com', 'https://stg-tsumiage.magicgifted.com', 'http://localhost:3000', 'http://localhost:3001'],
+    'allowed_origins' => ['https://tsumiage.magicgifted.com', 'https://stg-tsumiage.magicgifted.com', 'http://localhost:3000', 'http://localhost:3001', 'http://3.105.106.52'],
 
     'allowed_origins_patterns' => [],
 

bd1e37a9276410845dd4a05c92e0b66f35299173
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2025-10-05 21:51:05 +0900

    fix

--- config/cors.php
+++ config/cors.php
@@ -2,21 +2,41 @@
 
 return [
 
+    // 対象のパス
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
+    // 許可するHTTPメソッド
     'allowed_methods' => ['*'],
 
-    // TODO
-    'allowed_origins' => ['https://tsumiage.magicgifted.com', 'https://stg-tsumiage.magicgifted.com', 'http://localhost:3000', 'http://localhost:3001', 'http://3.105.106.52', 'http://tsumiage.online', 'https://tsumiage.online'],
+    // 許可するオリジン（ワイルドカード使用不可 if credentials=true）
+    'allowed_origins' => [
+        'https://tsumiage.magicgifted.com',
+        'https://stg-tsumiage.magicgifted.com',
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://3.105.106.52',
+        'http://tsumiage.online',
+        'https://tsumiage.online',
+    ],
 
-    'allowed_origins_patterns' => [],
+    // ポート付きや動的ドメイン対応
+    'allowed_origins_patterns' => [
+        '/^https:\/\/tsumiage\.magicgifted\.com(:[0-9]+)?$/',
+        '/^https:\/\/stg-tsumiage\.magicgifted\.com(:[0-9]+)?$/',
+        '/^https:\/\/tsumiage\.online(:[0-9]+)?$/',
+        '/^http:\/\/localhost(:[0-9]+)?$/',
+    ],
 
+    // 許可するリクエストヘッダ
     'allowed_headers' => ['*'],
 
+    // フロントに返す追加ヘッダ
     'exposed_headers' => [],
 
+    // プリフライトキャッシュの有効期限（秒）
     'max_age' => 0,
 
+    // Cookieや認証情報を送信する場合はtrue
     'supports_credentials' => true,
 
 ];

0573f9c32b357b6573ad171ac86bbe481b309e20
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2025-10-31 14:59:59 +0900

    fix

--- app/Domains/AuthTest/Services/AuthTestService.php
+++ app/Domains/AuthTest/Services/AuthTestService.php
@@ -4,22 +4,22 @@ declare(strict_types=1);
 
 namespace App\Domains\AuthTest\Services;
 
-use App\Domains\AuthTest\Queries\AuthTestQuery;
 use App\Domains\Shared\Auth\Services\AuthService;
 use App\Domains\Shared\LoginInfo\Entities\LoginInfoEntity;
+use App\Domains\Shared\User\Services\UserService;
 use App\Http\Exceptions\AppHttpException;
 use Illuminate\Http\Request;
 
 class AuthTestService
 {
     public function __construct(
-        private readonly AuthTestQuery $query,
+        private readonly UserService $userService,
         private readonly AuthService $authService,
     ) {}
 
     public function testAuth(Request $request): LoginInfoEntity
     {
-        $loginInfoModel = $this->query->getTestUserModel();
+        $loginInfoModel = $this->userService->getUserByEmail("user1@gmail.com");
         if (! $loginInfoModel) {
             throw new AppHttpException(404, 'テストユーザーが見つかりませんでした');
         }

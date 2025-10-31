b4a258baecaadef99133141269f1359c7cbe407c
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2025-10-31 14:21:49 +0900

    fix

--- routes/api.php
+++ routes/api.php
@@ -44,7 +44,7 @@ Route::middleware(['web'])->group(function () {
 
         // user（認証後）
         Route::get('/user/auth/bearer', [AuthBearerController::class, 'index']);
-        Route::post('/user/auth/email/verify/send', [AuthEmailVerifyController::class, 'index']); // TODO ブラウザが違う、PWAの場合にバグ
+        Route::post('/user/auth/email/verify/send', [AuthEmailVerifyController::class, 'index']);
         Route::delete('/user/delete', [UserDeleteController::class, 'index']);
 
         // task
